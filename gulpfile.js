6a586cfd70e2326947d83507031ab75a9434d034
Megumi Nakayama <m-nakayama@plate.co.jp>
2016-09-08 12:15:42 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var frontnote = require("gulp-frontnote");
+var uglify = require("gulp-uglify");
+var browser = require("browser-sync");
+var plumber = require("gulp-plumber");
+
+
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+gulp.task("js", function() {
+    gulp.src(["js/**/*.js","!js/min/**/*.js"])
+        .pipe(plumber())
+        .pipe(frontnote({
+            css: '../css/style.css'
+          }))
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("./css"))
+        .pipe(browser.reload({stream:true}));
+});
+
+gulp.task("sass", function() {
+    gulp.src("sass/**/*scss")
+        .pipe(plumber())
+        .pipe(frontnote())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("./css"))
+        .pipe(browser.reload({stream:true}))
+});
+
+
+gulp.task("default",['server'], function() {
+    gulp.watch(["js/**/*.js","!js/min/**/*.js"],["js"]);
+    gulp.watch("sass/**/*.scss",["sass"]);
+});
